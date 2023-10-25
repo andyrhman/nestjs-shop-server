87f2b280f756993a89bfd36daf21dccf0162322c
andyrhman <andyrahman989@gmail.com>
2023-10-25 18:57:48 +0800

    Added sort cart by the date and completed

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -1,29 +1,93 @@
-import { BadRequestException, Body, Controller, Delete, ForbiddenException, Get, NotFoundException, Param, Post, Req, UseFilters } from '@nestjs/common';
+import { Body, Controller, Delete, ForbiddenException, Get, NotFoundException, Param, Post, Req, UseGuards } from '@nestjs/common';
 import { CartService } from './cart.service';
 import { CreateCartDTO } from './dto/create.dto';
 import { Cart } from './models/cart.entity';
 import { AuthService } from 'src/auth/auth.service';
 import { Request } from 'express';
-import { HttpExceptionFilter } from 'src/http.exception';
+import { AuthGuard } from 'src/auth/auth.guard';
 
-@Controller('cart')
+@Controller()
 export class CartController {
     constructor(
         private cartService: CartService,
         private authService: AuthService
-    ) {}
+    ) { }
+
+    // * Get all user carts
+    @UseGuards(AuthGuard)
+    @Get('admin/carts')
+    async getCarts(
+        @Req() request: Request
+    ) {
+        let carts = await this.cartService.find({}, ['user']);
+
+        if (request.query.search) {
+
+            const search = request.query.search.toString().toLowerCase();
+
+            carts = carts.filter(cart => {
+                const userMatches = cart.user && (
+                    cart.user.fullName.toLowerCase().includes(search) ||
+                    cart.user.username.toLowerCase().includes(search) ||
+                    cart.user.email.toLowerCase().includes(search)
+                );
+
+                const productMatches = cart.product_title.toLowerCase().includes(search);
+
+                return productMatches || userMatches;
+            });
+        }
+        if (request.query.sort === 'newest' || request.query.sort === 'oldest') {
+            carts.sort((a, b) => {
+                if (request.query.sort === 'newest') {
+                    return new Date(b.created_at).getTime() - new Date(a.created_at).getTime();
+                } else {
+                    return new Date(a.created_at).getTime() - new Date(b.created_at).getTime();
+                }
+            });
+        }
+        if (request.query.sortByCompleted || request.query.sortByDate) {
+            const sortByCompleted = request.query.sortByCompleted?.toString().toLowerCase();
+            const sortByDate = request.query.sortByDate?.toString().toLowerCase();
+        
+            carts.sort((a, b) => {
+                if (sortByCompleted) {
+                    if (sortByCompleted === 'asc') {
+                        if (a.completed !== b.completed) {
+                            return a.completed ? 1 : -1;
+                        }
+                    } else {
+                        if (a.completed !== b.completed) {
+                            return a.completed ? -1 : 1;
+                        }
+                    }
+                }
+        
+                if (sortByDate) {
+                    if (sortByDate === 'newest') {
+                        return new Date(b.created_at).getTime() - new Date(a.created_at).getTime();
+                    } else {
+                        return new Date(a.created_at).getTime() - new Date(b.created_at).getTime();
+                    }
+                }
+        
+                return 0;
+            });
+        }
+        
+        
+        return carts;
+    }
 
     // * Add products to cart
-    // TODO -> If the cart is completed don't increment the quantity
-    // TODO -> Fix it by creating another cart.
-    @Post()
+    @Post('cart')
     async create(
         @Body() body: CreateCartDTO,
         @Req() request: Request
-    ){
+    ) {
         const user = await this.authService.userId(request);
         const existingProduct = await this.cartService.findLatestUncompletedProduct(body.product_id, user);
-        
+
         if (existingProduct) {
             if (existingProduct.completed === true) {
                 const c = new Cart();
@@ -32,7 +96,7 @@ export class CartController {
                 c.price = body.price;
                 c.product_id = body.product_id;
                 c.user_id = user
-    
+
                 return this.cartService.create(c);
             } else {
                 existingProduct.quantity += body.quantity;
@@ -45,20 +109,18 @@ export class CartController {
             c.price = body.price;
             c.product_id = body.product_id;
             c.user_id = user
-    
+
             return this.cartService.create(c);
         }
     }
-    
-    
 
     // * Get authenticated user products cart
-    @Get()
+    @Get('cart')
     async get(
         @Req() request: Request
-    ){
+    ) {
         const user = await this.authService.userId(request);
-        const cart = await this.cartService.find({user_id: user}, ['order']);
+        const cart = await this.cartService.find({ user_id: user }, ['order']);
         if (!cart) {
             throw new NotFoundException()
         }
@@ -66,15 +128,15 @@ export class CartController {
     }
 
     // * Delete Cart
-    @Delete(':cart_id')
+    @Delete('cart/:cart_id')
     async delete(
         @Param('cart_id') cart_id: string,
         @Req() request: Request
-    ){
+    ) {
         const user_id = await this.authService.userId(request);
-        const cart = await this.cartService.findOne({id: cart_id});
+        const cart = await this.cartService.findOne({ id: cart_id });
 
-        if(cart.user_id !== user_id){
+        if (cart.user_id !== user_id) {
             throw new ForbiddenException();
         }
         return this.cartService.deleteUserCart(user_id, cart_id);
