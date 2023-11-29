45eb27446598271b99889ca8db3d5c154ad74c6b
Andy <andyrhman@andyrahman989@gmail.com>
2023-11-29 20:27:17 +0800

    Test number 4

--- src/order/models/order-item.entity.ts
+++ src/order/models/order-item.entity.ts
@@ -30,6 +30,7 @@ export class OrderItem {
         type: 'enum',
         enum: OrderItemStatus,
         default: OrderItemStatus.SedangDikemas,
+        enumName: "order_items_status_enum" // Add this line
     })
     status: OrderItemStatus;
 
